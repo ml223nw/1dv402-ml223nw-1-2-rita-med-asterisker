72903d5551d16ca4cd237080cf19ebe3399c96c3
Mathias Lundberg <ml223nw@student.lnu.se>
2014-09-22 10:05:59 +0200

    Solved. Only need to add comments.

---
+++ 1-2-rita-med-asterisker/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_2_rita_med_asterisker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int caseSwitch = 0;
+
+            for (int row = 0; row < 25; row ++)
+            {
+                if (caseSwitch > 2)
+                {
+                    caseSwitch = 0;
+                }
+
+                switch (caseSwitch)
+                    {
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        break;
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    }
+
+                ++caseSwitch;
+                                
+                for (int column = 0; column < 39; column ++)
+                {                   
+                    Console.Write("* ");
+                }
+                
+                Console.WriteLine();
+                
+                if (row % 2 == 0)
+                {
+                    Console.Write(" ");
+                }                                    
+             }
+             Console.ResetColor();
+          }
+
+     }
+
+}
